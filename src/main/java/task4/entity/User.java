df064d45140cbb40f7c638938ab1109f06860bc4
Andrey <PolyosmakAK@gmail.com>
2025-07-07 23:29:34 +0300

    Задание 5

---
+++ src/main/java/task4/entity/User.java
@@ -0,0 +1,29 @@
+package task4.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(name = "id")
+    Long id;
+    @Column(name = "username")
+    String username;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
